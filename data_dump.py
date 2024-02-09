c14e4969c93e9aa2d6cd62e586cac02b803141ab
ishawant kumar <ishawantkumar@gmail.com>
2024-02-09 11:52:37 +0530

    data uploaded mongo

---
+++ data_dump.py
@@ -0,0 +1,21 @@
+import pymongo # pip install pymongo
+import pandas as pd
+import json
+
+client = pymongo.MongoClient("mongodb+srv://pawan:pawan123@cluster0.qafja9l.mongodb.net/?retryWrites=true&w=majority")
+
+DATA_FILE_PATH = (r"notebooks/Visadataset.csv")
+DATABASE_NAME = "US_VISA"
+COLLECTION_NAME = "visa_data"
+
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    df.reset_index(drop = True, inplace = True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
