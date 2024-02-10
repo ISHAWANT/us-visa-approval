1690f6780c68fde72999731a071d4734f9453d7b
ishawant kumar <ishawantkumar@gmail.com>
2024-02-10 17:46:09 +0530

    data validation updated

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -31,3 +31,5 @@ Data Validation realted contant start with DATA_VALIDATION VAR NAME
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
