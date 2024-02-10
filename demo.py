e50b986204d54fd81578bebf8dd1fa8110a26952
ishawant kumar <ishawantkumar@gmail.com>
2024-02-10 19:35:43 +0530

    model training completed

--- demo.py
+++ demo.py
@@ -7,16 +7,18 @@
 # ins = S3Client()
 
 #Data ingestion 
-from us_visa.entity.config_entity import DataIngestionConfig,DataTransformationConfig
+from us_visa.entity.config_entity import DataIngestionConfig,DataTransformationConfig,ModelTrainerConfig
 from us_visa.entity.config_entity import DataValidationConfig
 
 from us_visa.components.data_ingestion import DataIngestionArtifact
 from us_visa.components.data_validation import DataValidationArtifact
 from us_visa.components.data_transformation import DataTransformationArtifact
+from us_visa.components.model_trainer import ModelTrainerArtifact
 
 from us_visa.components.data_ingestion import DataIngestion
 from us_visa.components.data_validation import DataValidation
 from us_visa.components.data_transformation import DataTransformation
+from us_visa.components.model_trainer import ModelTrainer
 
 
 di_ins = DataIngestion(DataIngestionConfig)
@@ -33,3 +35,10 @@ dv_art = dv_ins.initiate_data_validation()
 dt_ins = DataTransformation(data_ingestion_artifact=di_art, data_transformation_config=DataTransformationConfig, data_validation_artifact=dv_art)
 
 dt_art = dt_ins.initiate_data_transformation()
+
+# Model trainer
+
+mt_ins = ModelTrainer(data_transformation_artifact=dt_art, model_trainer_config=ModelTrainerConfig)
+
+mt_art = mt_ins.initiate_model_trainer()
+
